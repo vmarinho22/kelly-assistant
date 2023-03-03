a5a84a9c6a0ca6b1c5391fbc62ae532022136b35
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2023-03-03 20:18:23 -0300

    fix: fixing transpilation api timeout error checking if audio is empty

--- src/classes/Assistant.py
+++ src/classes/Assistant.py
@@ -20,11 +20,15 @@ class Assistant:
         """
         
         audio_data = self.speech_recognition.listen(log_message)
-        audio_transpiled = self.speech_recognition.transpile_audio(audio_data)
         
-        return audio_transpiled
+        # Check if audio is null or in silence
+        if audio_data.frame_data != b'' or audio_data.frame_data != b'\x00' * len(audio_data.frame_data):
+            audio_transpiled = self.speech_recognition.transpile_audio(audio_data)
+            return audio_transpiled
+        
+        return ''
     
-    def speak(self, fileName: str, text: str = None) -> None:
+    def speak(self, file_name: str, text: str = None) -> None:
         """
             Method to speak audio from internal file.
             
@@ -32,8 +36,8 @@ class Assistant:
             audio.
         """
         if text:
-            self.audio_system.create_audio_by_text(fileName, text)
-        self.audio_system.play_audio(fileName)    
+            self.audio_system.create_audio_by_text(file_name, text)
+        self.audio_system.play_audio(file_name)    
         
     def is_called(self, text: str) -> bool:
         return 'kelly' in text.lower()
