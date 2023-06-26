0c1152eb5a2b4506e2f700779260ccb5cc65793f
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2023-06-26 16:27:25 -0300

    test: adding more tests to assistant module

--- src/modules/tests/test_Assistant.py
+++ src/modules/tests/test_Assistant.py
@@ -2,16 +2,16 @@ import pytest
 from src.modules.Assistant import Assistant
 from unittest.mock import MagicMock
 
+assistant = Assistant()
+
 class FrameData:
     def init(self):
         self.frame_data = ""
 
 def test_smoke():
-    assistant = Assistant()
     assert isinstance(assistant, Assistant)
 
 def test_listen_method(mocker):
-    assistant = Assistant()
 
     mock_frame_data = MagicMock(spec=FrameData())
     mock_frame_data.frame_data = b'frame_data'
@@ -24,3 +24,41 @@ def test_listen_method(mocker):
                  mock_transpile_audio_method)
     
     assert assistant.listen('log_message') == 'texto de retorno'
+
+def test_listen_method_with_empty_returned_text(mocker):
+
+    mock_frame_data = MagicMock(spec=FrameData())
+    mock_frame_data.frame_data = b''
+
+    mock_listen_method = MagicMock(return_value=mock_frame_data)
+    mock_transpile_audio_method = MagicMock(return_value='')
+
+    mocker.patch('src.modules.SpeechRecognition.listen', mock_listen_method)
+    mocker.patch('src.modules.SpeechRecognition.transpile_audio', \
+                 mock_transpile_audio_method)
+    
+    assert assistant.listen('log_message') == ''
+
+def test_speak_method_with_no_text(mocker):
+
+    mocker.patch('src.modules.AudioSystem.play_audio', MagicMock())
+
+    assert assistant.speak('init') is None
+
+def test_speak_method_with_custom_text(mocker):
+
+    mock_create_audio_by_text_method = MagicMock(return_value='text')
+
+    mocker.patch('src.modules.AudioSystem.create_audio_by_text', mock_create_audio_by_text_method)
+    mocker.patch('src.modules.AudioSystem.play_audio', MagicMock())
+
+    assert assistant.speak('init', 'test') is None
+
+def test_is_called_method():
+    assert assistant.is_called('kelly') is True
+    assert assistant.is_called('') is False
+
+def test_removeAssistantNameOfCommand():
+    assert assistant.removeAssistantNameOfCommand('kelly test') == 'test'
+    assert assistant.removeAssistantNameOfCommand('test') == 'test'
+    assert assistant.removeAssistantNameOfCommand('kelly') == ''
