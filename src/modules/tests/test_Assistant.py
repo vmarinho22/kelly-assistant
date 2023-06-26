2907699ebd2bda0631fb40771dfdb185c47757dc
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2023-06-26 16:08:54 -0300

    feat: tranform mock method

--- src/modules/tests/test_Assistant.py
+++ src/modules/tests/test_Assistant.py
@@ -1,6 +1,6 @@
 import pytest
 from src.modules.Assistant import Assistant
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock
 
 class FrameData:
     def init(self):
@@ -10,15 +10,17 @@ def test_smoke():
     assistant = Assistant()
     assert isinstance(assistant, Assistant)
 
-def test_listen_method():
+def test_listen_method(mocker):
     assistant = Assistant()
-    
+
     mock_frame_data = MagicMock(spec=FrameData())
     mock_frame_data.frame_data = b'frame_data'
-    
+
     mock_listen_method = MagicMock(return_value=mock_frame_data)
     mock_transpile_audio_method = MagicMock(return_value='texto de retorno')
 
-    with patch('src.modules.SpeechRecognition.listen', mock_listen_method), \
-        patch('src.modules.SpeechRecognition.transpile_audio', mock_transpile_audio_method):
-        assert assistant.listen('log_message') == 'texto de retorno'
+    mocker.patch('src.modules.SpeechRecognition.listen', mock_listen_method)
+    mocker.patch('src.modules.SpeechRecognition.transpile_audio', \
+                 mock_transpile_audio_method)
+    
+    assert assistant.listen('log_message') == 'texto de retorno'
